b5c64bd7897c41444d70532678572469edead841
Benjamin Schollnick <Benjamin@schollnick.net>
2015-03-20 12:13:44 -0400

    Importing from Odds-And-Ends
    * Mainly a re-release from v1.01 to a separate repository
    * Includes some minor updates
    * Moving a totally separate repository to allow easier managing of
    repository tags.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='Directory_Caching',
+    version='1.0.5',
+    url='https://github.com/bschollnick/Directory_Caching',
+    description = 'A Caching library for Directories & Files',
+    author='Benjamin Schollnick',
+    author_email='benjamin@schollnick.net',
+    packages=find_packages(),
+    install_requires=required,
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        ],
+    keywords = ['caching', 'files', 'directories', 'scandir', 'naturalsort'], # arbitrary keywords
+)
