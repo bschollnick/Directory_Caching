94612b56cb3a8a08a576c13423b08cbff647de79
Benjamin Schollnick <Benjamin@schollnick.net>
2016-05-02 18:24:56 -0400

    Expanded DocString documentation and standardized variable names
    * Changed magic to signature
    * changed proper_extension to extensions
    * eliminated file_type

--- directory_caching/archives2.py
+++ directory_caching/archives2.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Unified Archive support for Python.
 
@@ -13,8 +14,8 @@ The current model, is expanded from:
 http://stackoverflow.com/questions/13044562/
     python-mechanism-to-identify-compressed-file-type-and-uncompress
 
-Which gave me the spark on a sensible manner to deal with signature and managing via
-the object model.
+Which gave me the spark on a sensible manner to deal with signature and
+managing via the object model.
 
 More File signatures are avialable here:
 
@@ -28,7 +29,8 @@ Mimetype Information from here:
 Suggested workflow:
 
 1) Initialize by using id_cfile_by_signature
-    a) This will a populated class (with filename), archive listings will not be populated
+    a) This will a populated class (with filename), archive listings will not
+       be populated
 
 2) Use the get_listings function to get the listings for the compressed file
 3) Use extract_mem_file to pull out individual files, as needed.
@@ -38,8 +40,8 @@ Next steps:
 Create a batch extractor?  To increase efficiency?
 
 Zip, RAR are supported, what other formats might be useful?
-    ?? PDF? - Technically not a archive, and will need to use Ghostscript to create
-                extracted pages
+    ?? PDF? - Technically not a archive, and will need to use Ghostscript to
+              create extracted pages
 """
 
 #		Gallery Comic Support
@@ -51,104 +53,185 @@ import os.path
 
 class NotInitializedYet(exceptions.Exception):
     """
-    d
+    General Purpose Exception Stub.
+
+    Called if self.handler in CompressedFile has not been
+    assigned, yet, the code has been asked to process (eg Open) the
+    compressed file.
     """
     pass
 
 class CompressedFile(object):
+    r"""
+    The mainstay of the Archive2 code.
+
+    This object is the framework that the inherited archive types are
+    based off of.
+
+    extensions - is a list of the file extensions that the archive is known
+                 to use.  For example, a zip file would be ['zip', 'cbz'].
+                 Currently, this is not used, but in the future it maybe.
+
+                 - Possible future uses:
+                     - Check for archive by filename (with verification by
+                       signature done after matching by filename)
+
+    mime_type - The standard mimetype for the archive ()
+                For example, for zip, it is 'compressed/zip'.
+                Can be used, to identify the mimetype to a web server.
+
+    filename - Fully qualified local system Filename to the archive
+               (eg c:\users\JohnCleese\archives\scripts101.zip)
+
+    listings - default value []
+
+               This contains the listings of the archive file.
+
+               **This is populated by the get_listings() function.**
+
+               This is somewhat non-intuitive.  But the logic is that
+               the programmer may want to add, remove, etc.  Open just
+               opens the the file, so that you can manipulate the
+               underlying archive file.
+
+    handler - Is the "pointer" to the archiver function used to manipulate
+              the archive file.  For example:
+
+                  zip file - zipfile.ZipFile
+                  rar file - rarfile.RarFile
+
+              **This is normally populated by the id_cfile_by_sig function.**
+
+    signature - This is the "magic" signature that is used to identify
+                the archive file.  It is used to compare the first xx bytes
+                of the archive.  If it matches, then it is that type of
+                archive.
+
+Example:
+
+filename='test.zip'
+archive_file = archives2.id_cfile_by_sig(filename)
+archive_file.get_listings()
+print archive_file.listings
+print filename, 'is a', cf.mime_type, 'file'
     """
-d
-    """
-    file_type = None
+    extensions = None
     mime_type = None
-    proper_extension = None
     filename = None
     listings = []
     handler = None
-    magic = None
+    signature = None
 
     def __init__(self, filename):
         """
-d
+        filename (str): The Fully Qualified Filename to the archive file
+
+        :param filename: The Fully Qualified Filename to the archive file
+        :type filename: string
+
+        Initialization routines for Archive2.
+
+        Inputs -
+                 Filename - The fully qualified filename for the archive file
+
+        Returns - None
         """
-        # f is an open file or file like object, or filename
         self.filename = os.path.realpath(filename)
-#        self.accessor = self.open()
-        self.fhandle = None
-        self.identified = None
+        self.handler = None
         self.listings = []
 
     @classmethod
-    def is_magic(cls, data):
+    def is_signature(cls, data):
         """
-d
-        """
-        return data.startswith(cls.magic)
+        data (bool): The signature bytes from the archive file.
 
-    def open(self):
+        Checks the archive signature (self.signature) against the xxx bytes
+        from the file header.
+
+        If they match, returns True, else Returns False.
         """
-d
+        return data.startswith(cls.signature)
+
+    def _open(self):
         """
+        Attempts to use self.handler to open the file.
+        self.handler is normally set automatically by id_cfile_by_sig.
+        If self.handler is not set, then it will raise NotInitializedYet.
+
+        Inputs - None
+
+        Returns - Handler as assigned by self.handler
+       """
+        if self.handler != None:
 # pylint: disable=E1102
-        if self.handler == None:
-            raise NotInitializedYet
-        else:
             return self.handler(self.filename)
-#    magic = None
 # pylint: enable=E1102
+        else:
+            raise NotInitializedYet
+
+    def return_mime(self):
+        """
+        Return the stored mimetype for the archive file.
+
+        Inputs - None
+        Returns - If a recognized archive file is loaded, the mimetype of
+                  said archive.  Otherwise, None.
+        """
+        if self.mime_type != None:
+            return self.mime_type
+        else:
+            raise NotInitializedYet
+
 
     def get_listings(self):
         """
-d
+        Load the listings from the archive into self.listings.
+
+        inputs - None
+        returns - None
         """
-        if self.handler == None:
-            raise NotInitializedYet
-        else:
-            self.listings = []
-# pylint: disable=E1102
-            with self.handler(self.filename, 'r') as cfile:
-# pylint: enable=E1102
-                for afn in cfile.namelist():
-                    if not (afn.startswith("__MACOSX") or os.path.split(afn)[1] == ""):
-                        self.listings.append(afn)
-                return True
-            return False
+        self.listings = []
+        handle = self._open()
+        with handle(self.filename, 'r') as cfile:
+            for afn in cfile.namelist():
+                if not (afn.startswith("__MACOSX") or
+                        os.path.split(afn)[1] == ""):
+                    self.listings.append(afn)
+            return True
+        return False
 
     def extract_mem_file(self, filename):
         """
-d
-        """
-# pylint: disable=E1102
-        if self.handler == None:
-            raise NotInitializedYet
-        else:
-            with self.handler(self.filename, 'r') as cfile:
-                return cfile.read(filename)
-# pylint: enable=E1102
+        Extract filename out of the archive, and return it as a blob.
 
+        inputs - filename to extract
+        returns - blob from the archive.
+        """
+        handle = self._open()
+        with handle(self.filename, 'r') as cfile:
+            return cfile.read(filename)
 
 
 class ZIPFile(CompressedFile):
     """
-d
+    Support Class stub for ZIP file support
     """
     import zipfile
-    magic = '\x50\x4b\x03\x04'
-    file_type = ['zip', 'cbz']
+    signature = '\x50\x4b\x03\x04'
+    extensions = ['zip', 'cbz']
     mime_type = 'compressed/zip'
     handler = zipfile.ZipFile
 
 
-
 class RarFile(CompressedFile):
     """
-d
+    Support Class stub for RAR file support
     """
     import rarfile
     rarfile.PATH_SEP = '/'
-    magic = '\x52\x61\x72\x21\x1a\x07'
+    signature = '\x52\x61\x72\x21\x1a\x07'
 
-    file_type = ['rar', 'cbr']
+    extensions = ['rar', 'cbr']
     mime_type = 'application/x-rar-compressed'
     handler = rarfile.RarFile
 
@@ -158,13 +241,36 @@ ARCHIVE_CLASSES = [ZIPFile, RarFile]  # BZ2File, GZFile,
 # factory function to create a suitable instance for accessing files
 def id_cfile_by_sig(filename):
     """
-d
+    Effectively the core function of the module.
+
+    It established and configures the archive functionality for the
+    filename passed to it.
+
+    Inputs - Fully qualified local filepathname of the archive file
+
+    Returns - The initialized archive class, that is configured to work
+              with the archive (eg. ZIPFile class, or RarFile)
+
+    This function will read 128 bytes from the beginning of the file
+    and then step through the ARCHIVE_CLASSES, checking the signature
+    of each ARCHIVE_CLASSES, against the file contents.
+
+    If it finds a match, it will then return that class with the proper
+    filename.
+
+Example:
+
+filename='test.zip'
+archive_file = archives2.id_cfile_by_sig(filename)
+archive_file.get_listings()
+print archive_file.listings
+print filename, 'is a', cf.mime_type, 'file'
     """
     with file(filename, 'rb') as cfile:
         start_of_file = cfile.read(128)
         cfile.seek(0)
         for cls in ARCHIVE_CLASSES:
-            if cls.is_magic(start_of_file):
+            if cls.is_signature(start_of_file):
                 return cls(filename)
         return None
 
